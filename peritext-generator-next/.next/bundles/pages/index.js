f923d134faf666614a4e35f60110bdafdedfb2d0
Robin de Mourat <robin.demourat@gmail.com>
2017-09-03 10:47:14 +0200

    update dependencies

--- peritext-generator-next/.next/bundles/pages/index.js
+++ peritext-generator-next/.next/bundles/pages/index.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/', function() {
-            var comp = module.exports=webpackJsonp([9],{1289:function(e,t,u){e.exports=u(1290)},1290:function(e,t,u){"use strict";var n=function(e){return e&&e.__esModule?e:{default:e}}(u(1));Object.defineProperty(t,"__esModule",{value:!0});var l=function(e){return e&&e.__esModule?e:{default:e}}(u(142)),o=u(95).templates.web.Home;t.default=function(){return n.default.createElement(l.default,{activeViewId:"home"},n.default.createElement(o,null))}}},[1289]);
+            var comp = module.exports=webpackJsonp([9],{2009:function(e,t,u){e.exports=u(762)},762:function(e,t,u){"use strict";var n=function(e){return e&&e.__esModule?e:{default:e}}(u(1));Object.defineProperty(t,"__esModule",{value:!0});var l=function(e){return e&&e.__esModule?e:{default:e}}(u(154)),o=u(126).templates.web.Home;t.default=function(){return n.default.createElement(l.default,{activeViewId:"home"},n.default.createElement(o,null))}}},[2009]);
             return { page: comp.default }
           })
         
--- peritext-generator-next/.next/bundles/pages/section.js
+++ peritext-generator-next/.next/bundles/pages/section.js
@@ -1,6 +1,6 @@
 
           window.__NEXT_REGISTER_PAGE('/section', function() {
-            var comp = module.exports=webpackJsonp([8],{1295:function(e,t,u){e.exports=u(1296)},1296:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}var i=function(e){return e&&e.__esModule?e:{default:e}}(u(1));Object.defineProperty(t,"__esModule",{value:!0});var a=n(u(155)),d=n(u(142)),o=u(95),r=n(u(509)),l=o.templates.web.Section;t.default=function(e){var t=e.url.query;return t.contextualizationId&&setTimeout(function(){a.default.replace("/section?id="+t.id,"/section/"+t.id)},1e3),i.default.createElement(d.default,{activeViewId:t.id},i.default.createElement(l,{id:t.id,citations:r.default,query:t}))}}},[1295]);
+            var comp = module.exports=webpackJsonp([8],{2011:function(e,t,u){e.exports=u(764)},764:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}var i=function(e){return e&&e.__esModule?e:{default:e}}(u(1));Object.defineProperty(t,"__esModule",{value:!0});var a=n(u(212)),d=n(u(154)),o=u(126),r=n(u(585)),l=o.templates.web.Section;t.default=function(e){var t=e.url.query;return t.contextualizationId&&setTimeout(function(){a.default.replace("/section?id="+t.id,"/section/"+t.id)},1e3),i.default.createElement(d.default,{activeViewId:t.id},i.default.createElement(l,{id:t.id,citations:r.default,query:t}))}}},[2011]);
             return { page: comp.default }
           })
         
