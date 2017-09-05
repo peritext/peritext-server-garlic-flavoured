7afee64ab30666e44fd286ab563698232c8c6688
Robin de Mourat <robin.demourat@gmail.com>
2017-09-05 18:01:48 +0200

    clean the code

--- server.js
+++ server.js
@@ -18,19 +18,13 @@ const express = require('express');
 // route handlers (see below)
 const citationLocales = require('./routeHandlers/citation-locales');
 const citationStyles = require('./routeHandlers/citation-styles');
-const oAuthProxy = require('./routeHandlers/oauth-proxy');
-const gistStory = require('./routeHandlers/gist-story');
 const renderStory = require('./routeHandlers/render-story');
 
 let config;
 // in production mode config variables must be set as environment variables
 if (process.env.NODE_ENV === 'production') {
   config = {
-    github_client_id: process.env.GITHUB_CLIENT_ID,
-    github_client_secret: process.env.GITHUB_CLIENT_SECRET,
     port: process.env.PORT || 3000,
-    adminUserName: process.env.ADMIN_USERNAME,
-    adminPassword: process.env.ADMIN_PASSWORD
   };
 }
 // in development mode config variables are retrieved from a json file
@@ -58,22 +52,11 @@ module.exports = app;
  * Routes definitions and routes handlers are separated
  * to facilitate further changes in the server's api
  */
-// app.get('/presentations/:id?', presentationsRoutes.getPresentations);
-// app.post('/presentations/:id', presentationsRoutes.updatePresentation);
-// app.put('/presentations/:id', presentationsRoutes.createPresentation);
-// app.delete('/presentations/:id', presentationsRoutes.deletePresentation);
-
-// app.get('/stories/:id?', storiesRoutes.getStories);
-// app.post('/stories/:id', storiesRoutes.updateStory);
-// app.put('/stories/:id', storiesRoutes.createStory);
-// app.delete('/stories/:id', storiesRoutes.deleteStory);
 
 // app.post('/render-presentation', renderPresentation);
 app.post('/render-story', cors(), renderStory.createRenderingJob);
 app.get('/render-story', cors(), renderStory.getRenderingJob);
 
-// proxy used in the github oauth authentication process
-app.post('/oauth-proxy/:appName', cors(),  oAuthProxy);
 // citation data servers
 app.get('/citation-locales/:id?', citationLocales);
 app.get('/citation-styles/:id?', citationStyles);
